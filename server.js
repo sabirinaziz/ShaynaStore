ec191d05debeea5069030eab79eb70296b0bb420
sabirinaziz <azizsabirin98@gmail.com>
2021-04-03 22:41:22 +0700

    melakukan perubahan project final 1

--- server.js
+++ server.js
@@ -4,16 +4,10 @@ const path = require('path')
 
 const app = express()
 
-let server = require('http').Server(app);
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-	res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
-
 const port = process.env.PORT || 8080
-server.listen(port, () => {
-    console.log("App is running on port " + port);
-});
+app.listen(port)
+
+console.log("App is running on port " + port);
